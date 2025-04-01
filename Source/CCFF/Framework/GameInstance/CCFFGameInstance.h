cd0657416e7fbc64cf89638d14b2150766956f8c
9yutae <9yutae@gmail.com>
2025-04-01 16:18:10 +0900

    [Feat] Add TitleMap and initial UCCFFGameInstance setup
    #01 TitleMap 생성
    #02 UCCFFGameInstance 클래스 생성

---
+++ Source/CCFF/Framework/GameInstance/CCFFGameInstance.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CCFFGameInstance.generated.h"
+
+UCLASS()
+class CCFF_API UCCFFGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
