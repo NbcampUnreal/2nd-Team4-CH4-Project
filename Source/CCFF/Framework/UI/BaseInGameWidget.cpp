530391b0e85fab8c38ba9b3cb5bb8b8ce68f4946
choidahye <daheychoe2000@gmail.com>
2025-04-09 16:18:47 +0900

    [Update] Timer Widget UI
    1. 타미어 디자인 추가

--- Source/CCFF/Framework/UI/BaseInGameWidget.cpp
+++ Source/CCFF/Framework/UI/BaseInGameWidget.cpp
@@ -6,7 +6,7 @@ void UBaseInGameWidget::UpdateTimerText_Implementation(const float NewTime)
 {
 	int32 Minutes = FMath::FloorToInt(NewTime / 60.0f);
 	int32 Seconds = FMath::FloorToInt(NewTime) % 60;
-	FString FormattedTime = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+	FString FormattedTime = FString::Printf(TEXT("%02d : %02d"), Minutes, Seconds);
 
 	if (TimerText)
 	{
