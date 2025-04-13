d3a1f85321b8cd5ff756f4fa6cb5a873dfb795cf
KimSeungHwan <ksh951222@gmail.com>
2025-04-13 15:48:05 +0900

    [Add] Create Environmental Item Classes

---
+++ Source/CCFF/Items/Class/ExplosiveItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Class/EnvironmentItem.h"
+#include "ExplosiveItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CCFF_API AExplosiveItem : public AEnvironmentItem
+{
+	GENERATED_BODY()
+	
+};
