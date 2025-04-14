e7ef9222f3d88974d1c38931a942546f2408053f
9yutae <9yutae@gmail.com>
2025-04-14 18:47:59 +0900

    [Feat] Add Graphic Settings UI with Apply/Reset Support
    #01 ApplySettingsInterface 인터페이스 추가: Apply 및 Reset 기능을 통합 처리하기 위한 기반 구성
    #02 GraphicSettingsWidget 구현: 해상도 및 화면 모드 선택, 미리보기 반영, Apply/Reset/Cancel 기능 포함
    #03 SettingsWidget 수정: Apply/Reset 버튼에 대한 바인딩 구현 (기존 TODO 처리)

--- Source/CCFF/Framework/UI/GraphicSettingsWidget.h
+++ Source/CCFF/Framework/UI/GraphicSettingsWidget.h
@@ -1,17 +1,52 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Framework/UI/BaseUserWidget.h"
+#include "Framework/UI/Settings/ApplySettingsInterface.h"
 #include "GraphicSettingsWidget.generated.h"
 
-/**
- * 
- */
-UCLASS()
-class CCFF_API UGraphicSettingsWidget : public UBaseUserWidget
+UCLASS(config = GameUserSettings)
+class CCFF_API UGraphicSettingsWidget : public UBaseUserWidget, public IApplySettingsInterface
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeConstruct() override;
+
+	virtual void ApplySettings_Implementation() override;
+	virtual void ResetSettings_Implementation() override;
+
+	void ApplyPreviewSettings();
+	void RestoreCachedSettings();
+
+	UFUNCTION()
+	void OnWindowModeChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+	UFUNCTION()
+	void OnResolutionChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Settings", meta = (BindWidget))
+	class UComboBoxString* WindowModeComboBox;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Settings", meta = (BindWidget))
+	class UComboBoxString* ResolutionComboBox;
+
+private:
+	// Current Selected Value
+	EWindowMode::Type SelectedWindowMode;
+	FIntPoint SelectedResolution;
+
+	// Cached Value (Cancel)
+	FIntPoint CachedResolution;
+	EWindowMode::Type CachedWindowMode;
+
+	// Default Value (Reset)
+	const FIntPoint DefaultResolution = FIntPoint(1920, 1080);
+	const EWindowMode::Type DefaultWindowMode = EWindowMode::WindowedFullscreen;
+
+	void InitializeWindowModeOptions();
+	void InitializeResolutionOptions();
+	void UpdateComboBoxSelections();
+
 };
