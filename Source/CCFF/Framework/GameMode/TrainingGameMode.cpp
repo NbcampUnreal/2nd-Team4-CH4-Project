98513b4ddc7c070868ab2d837c28b39332fa4a0d
choidahye <daheychoe2000@gmail.com>
2025-04-16 00:31:07 +0900

    [fix] trainingRoom rotation
    1. 훈련장 회전 수정

--- Source/CCFF/Framework/GameMode/TrainingGameMode.cpp
+++ Source/CCFF/Framework/GameMode/TrainingGameMode.cpp
@@ -18,7 +18,57 @@ void ATrainingGameMode::BeginPlay()
 {
     Super::BeginPlay();
 
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	if (PC)
+	{
+		SpawnPlayer(PC);
+	}
+
     RegisterTrainingBotDamageEvents();
+}
+
+void ATrainingGameMode::SpawnPlayer(AController* NewPlayer)
+{
+	if (!NewPlayer) return;
+
+	FName SelectedID = NAME_None;
+	SelectedID = "Cactus";
+
+
+	if (!CharacterClasses.Contains(SelectedID))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("++++++++++++++++++[SpawnPlayer] '%s' no mapping"), *SelectedID.ToString());
+		return;
+	}
+	TSubclassOf<APawn> PawnClass = CharacterClasses[SelectedID];
+
+	AActor* StartSpot = ChoosePlayerStart(NewPlayer);
+	const FTransform StartTransform = StartSpot ? StartSpot->GetActorTransform() : FTransform::Identity;
+	const FVector SpawnLocation = StartTransform.GetLocation();
+	const float SpawnYaw = StartTransform.GetRotation().Rotator().Yaw;
+
+	const FRotator SpawnRotation(0.f, SpawnYaw, 0.f);
+
+	FActorSpawnParameters Params;
+	Params.Owner = NewPlayer;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	APawn* NewPawn = GetWorld()->SpawnActor<APawn>(PawnClass, SpawnLocation, SpawnRotation, Params);
+	if (NewPawn)
+	{
+		NewPlayer->Possess(NewPawn);
+
+		if (APlayerController* PC = Cast<APlayerController>(NewPlayer))
+		{
+			FRotator CR = PC->GetControlRotation();
+			CR.Yaw += 360.f;
+			PC->SetControlRotation(CR);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("++++++++++++++++++[SpawnPlayer] Spawn Fail : %s"), *PawnClass->GetName());
+	}
 }
 
 void ATrainingGameMode::RegisterTrainingBotDamageEvents()
